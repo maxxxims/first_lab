39cb9ad1412cacc0b350e7835f23295931c59840
maxxxims <78605460+maxxxims@users.noreply.github.com>
2021-10-09 19:19:31 +0300

    Add files via upload

---
+++ first_part/main.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+
+
+def do_first_task(files):
+    for file_name in files:
+        with open(file_name, 'r') as file:
+            n = int(file.readline())
+            x = []
+            y = []
+            for i in range(n):
+                line = [float(i) for i in file.readline().split(" ")]
+                x.append(line[0])
+                y.append(line[1])
+
+        R_max = max(max(x), max(y))
+        R_min = min(min(x), min(y))
+        plt.scatter(x,y, s=7, marker="D", c='yellow', linewidths=1, edgecolors='red', alpha=0.8)
+        plt.ylim([R_min - 10, R_max + 10])
+        plt.xlim([R_min - 10, R_max + 10])
+        plt.title(file_name + ' points')
+        plt.savefig('result_' + file_name[:3] + '.png')
+        plt.close()
+
+if __name__ == "__main__":
+    my_files = ['001.dat', '002.dat', '003.dat', '004.dat', '001.dat']
+    do_first_task(my_files)
