7de4631725b3017c85e3236eef8097d20b0c9275
Maxim <maxxxi_boyk@mail.ru>
2021-12-12 22:33:55 +0300

    issues is fixed

--- third_part/main.py
+++ third_part/main.py
@@ -8,18 +8,40 @@ result = [[0 for i in range(8)] for i in range(7)]
 result2 = [[0 for i in range(8)] for i in range(6)]
 
 
+pr = {}
+u = 0
+
+gr = {}
+y = 0
+
 with open(data, 'r') as file:
     for line in file:
-        prep_number = int(line.split(';')[0][-1]) - 1
+        #prep_number = int(line.split(';')[0][-1]) - 1
+        prep_ = line.split(';')[0]
+        if prep_ in list(pr.keys()):
+            pass
+        else:
+            pr[prep_] = u
+            u+=1
+        prep_number = pr[prep_]
         mark = int(line.split(';')[2]) - 3
         result[prep_number][mark] += 1
 
-        group_number = int(line.split(';')[1][-1]) - 1
+        #group_number = int(line.split(';')[1][-1]) - 1
+        group_ = line.split(';')[1]
+        if group_ in list(gr.keys()):
+            pass
+        else:
+            gr[group_] = y
+            y+=1
+        group_number = gr[group_]
         result2[group_number][mark] += 1
 
 
-preps = ['prep' + str(i) for i in range(1,8)]
-group = ['75' + str(i+1) for i in range(6)]
+#preps = ['prep' + str(i) for i in range(1,8)]
+preps = list(pr.keys())
+#group = ['75' + str(i+1) for i in range(6)]
+group = list(gr.keys())
 widht = 0.15
 
 x = np.arange(len(preps))
